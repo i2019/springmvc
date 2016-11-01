90e78fc1543347e1067e8b80e309d9fcaa1e32be
water.li <water.li@shijinet.com.cn>
2016-11-01 17:20:43 +0800

    demo

---
+++ src/main/java/ty/sprmvc/controller/Go.java
@@ -0,0 +1,23 @@
+package ty.sprmvc.controller;
+
+public class Go {
+	private String goMsg;
+	private String goName;
+	
+	public String getGoName() {
+		return goName;
+	}
+
+	public void setGoName(String goName) {
+		this.goName = goName;
+	}
+
+	public String getGoMsg() {
+		return goMsg;
+	}
+
+	public void setGoMsg(String goMsg) {
+		this.goMsg = goMsg;
+	}
+	
+}
--- src/main/java/ty/sprmvc/controller/GoController.java
+++ src/main/java/ty/sprmvc/controller/GoController.java
@@ -1,13 +1,25 @@
 package ty.sprmvc.controller;
 
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.PropertyAccessorFactory;
+import org.springframework.beans.PropertyValue;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class GoController {
+public class GoController implements EnvironmentAware{
 
+	private Environment environment;
+	
+	@Override
+	public void setEnvironment(Environment environment) {
+		this.environment=environment;
+	}
+	
 	//处理HEAD类型的“/”请求
 	@RequestMapping(value={"/"},method={RequestMethod.HEAD})
 	public String head(){
@@ -17,8 +29,16 @@ public class GoController {
 	//处理GET类型的“/index”请求
 	@RequestMapping(value={"/index"},method={RequestMethod.GET})
 	public String index(Model model){
-		model.addAttribute("msg", "Let's Go!");
+		
+		Go go=new Go();
+		BeanWrapper bw=PropertyAccessorFactory.forBeanPropertyAccess(go);
+		bw.setPropertyValue("goMsg", "欢迎");
+		PropertyValue propertyValue=new PropertyValue("goName","李江涛");
+		bw.setPropertyValue(propertyValue);
+		
+		model.addAttribute("msg", go.getGoMsg()+go.getGoName()+"！"+"Let's Go!");
 		return "WEB-INF/pages/go.jsp";
 	}
+
 	
 }
